5277f3af8c282db6a87a3b11469b211fbe6709eb
Eric Iacutone <eric.iacutone@gmail.com>
2014-08-31 19:27:33 -0400

    use rails for validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable
          
   has_many :links
   
   validate :phone_number_format
+  validates_presence_of :email, :name, :phone_number, :password_confirmation
+  validates_uniqueness_of :email
+  validates_numericality_of :phone_number
   
   def phone_number_format
     if self.phone_number?
-      phone = self.phone_number.scan(/\d+/).flatten.join('') 
+      phone = self.phone_number.gsub('-', '').scan(/\d+/).flatten.join('') 
       if phone.size != 10
         errors.add(:base, "Phone number needs to be 10 characters.")
       end
