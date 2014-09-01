0842df45c9efeefd26f703041f3f13eb1e84266b
Eric Iacutone <eric.iacutone@gmail.com>
2014-08-31 23:09:40 -0400

    stupid email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,6 +87,9 @@ OneTimeLink::Application.configure do
     }
   }
   
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
